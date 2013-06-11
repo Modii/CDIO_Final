7b7a4349f2b09cbd0eafaadc5af7dd340a84a2fa
Morten <Morten@10.16.100.136>
2013-06-11 09:44:28 +0200

    Gamle kode fra datakom del 2 indsat
    Signed-off-by: Morten <Morten@10.16.100.136>

--- 12_02324_F13-Final/src/java_ASE/Data.java
+++ 12_02324_F13-Final/src/java_ASE/Data.java
@@ -1,10 +1,79 @@
 package java_ASE;
 
 public class Data {
-
-	public static void main(String[] args) {
-
-
+	
+	String weightMsg, oprID, serverInput, itemName, userInput;
+	int itemNoInput, itemNoStore;
+	String[] splittedInput = new String[10];
+	double tara, netto, bruttoCheck;
+	
+	String getWeightMsg() {
+		return weightMsg;
 	}
+	void setWeightMsg(String weightMsg) {
+		this.weightMsg = weightMsg;
+	}
+	String getOprID() {
+		return oprID;
+	}
+	void setOprID(String oprID) {
+		this.oprID = oprID;
+	}
+	String getServerInput() {
+		return serverInput;
+	}
+	void setServerInput(String serverInput) {
+		this.serverInput = serverInput;
+	}
+	String getItemName() {
+		return itemName;
+	}
+	void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	String getUserInput() {
+		return userInput;
+	}
+	void setUserInput(String userInput) {
+		this.userInput = userInput;
+	}
+	int getItemNoInput() {
+		return itemNoInput;
+	}
+	void setItemNoInput(int itemNoInput) {
+		this.itemNoInput = itemNoInput;
+	}
+	int getItemNoStore() {
+		return itemNoStore;
+	}
+	void setItemNoStore(int itemNoStore) {
+		this.itemNoStore = itemNoStore;
+	}
+	String[] getSplittedInput() {
+		return splittedInput;
+	}
+	void setSplittedInput(String[] splittedInput) {
+		this.splittedInput = splittedInput;
+	}
+	double getTara() {
+		return tara;
+	}
+	void setTara(double tara) {
+		this.tara = tara;
+	}
+	double getNetto() {
+		return netto;
+	}
+	void setNetto(double netto) {
+		this.netto = netto;
+	}
+	double getBruttoCheck() {
+		return bruttoCheck;
+	}
+	void setBruttoCheck(double bruttoCheck) {
+		this.bruttoCheck = bruttoCheck;
+	}
+
+	
 
 }
--- 12_02324_F13-Final/src/java_ASE/Main.java
+++ 12_02324_F13-Final/src/java_ASE/Main.java
@@ -1,10 +1,32 @@
 package java_ASE;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class Main {
 
-	public static void main(String[] args) {
 
 
+
+	public static void main(String[] args) throws Exception { 
+
+		Sequences s = new Sequences();
+
+		Socket clientSocket = new Socket("169.254.2.3", 8000); 
+
+		DataOutputStream outToServer = 
+				new DataOutputStream(clientSocket.getOutputStream()); 
+
+		BufferedReader inFromServer = 
+				new BufferedReader(new
+						InputStreamReader(clientSocket.getInputStream())); 
+		
+
+		s.sequence1(inFromServer, outToServer);
+
+		clientSocket.close();
 	}
 
 }
